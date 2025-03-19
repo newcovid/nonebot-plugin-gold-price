32e5242af8c66d0abfeca59c5b774ee2c52868a2
newcovid <96228495+newcovid@users.noreply.github.com>
2025-03-19 23:16:57 +0800

    Update __init__.py

--- nonebot_plugin_gold_price/__init__.py
+++ nonebot_plugin_gold_price/__init__.py
@@ -8,7 +8,7 @@ __plugin_meta__ = PluginMetadata(
     description="提供实时金价查询、定时推送、金价预警功能。另外能够将历史查询数据进行存储并绘制折线图表。数据来源于金价API",
     usage="/goldprice x天; /金价 x天;  /金价 x年",
     type="application",
-    homepage="{项目主页}",
+    homepage="https://github.com/newcovid/nonebot-plugin-gold-price",
     config=Config,
     supported_adapters={"~onebot.v11"},
 
